668875b01d570e37e67703e15398618cb64fd77b
Logan Levin <loganjlevin@gmail.com>
2023-07-05 15:52:37 -0700

    Create add.py

---
+++ Exercise 1.1/add.py
@@ -0,0 +1,11 @@
+# Prompt the user to enter the first number
+a = int(input("Enter the first number: "))
+
+# Prompt the use to enter the second number
+b = int(input("Enter the second number: "))
+
+# Add the two numbers and store them in variable c
+c = a + b
+
+# Print the value of c
+print("The sum of ", a, "and", b, "is: ", c)
